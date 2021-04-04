5ba3a35b847cbe830d2054ca4de768664a57b9f2
t-sin <shinichi.tanaka45@gmail.com>
2021-04-04 23:47:09 +0900

    Clear articles in `blog`

--- .asha/blog/articles.lisp
+++ .asha/blog/articles.lisp
@@ -1,10 +1,2 @@
 
-#S(ARTICLE-SET
-   :NAME "blog"
-   :TEMPLATE-NAME "markdown-template"
-   :ARTICLES (#S(CONTENT
-                 :NAME "test"
-                 :TEMPLATE-NAME "markdown-template"
-                 :CREATED-AT ""
-                 :UPDATED-AT ""
-                 :PATHSTR "blog/test.md"))) 
+#S(ARTICLE-SET :NAME "blog" :TEMPLATE-NAME "markdown-template" :ARTICLES NIL) 
