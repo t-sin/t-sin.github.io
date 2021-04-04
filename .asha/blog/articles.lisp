9ebb690aa208766d47b9087c452cd18d650fd679
t-sin <shinichi.tanaka45@gmail.com>
2021-04-04 23:15:27 +0900

    Update state files

---
+++ .asha/blog/articles.lisp
@@ -0,0 +1,10 @@
+
+#S(ARTICLE-SET
+   :NAME "blog"
+   :TEMPLATE-NAME "markdown-template"
+   :ARTICLES (#S(CONTENT
+                 :NAME "test"
+                 :TEMPLATE-NAME "markdown-template"
+                 :CREATED-AT ""
+                 :UPDATED-AT ""
+                 :PATHSTR "blog/test.md"))) 
--- .asha/contents.lisp
+++ .asha/contents.lisp
@@ -1,5 +1,25 @@
 
-(#S(CONTENT :NAME "works" :TEMPLATE-NAME "markdown-template" :CREATED-AT "2021-03-31T18:00:00+09:00" :UPDATED-AT "" :PATHSTR "works.md")
- #S(CONTENT :NAME "about" :TEMPLATE-NAME "markdown-template" :CREATED-AT "2021-03-31T18:00:00+09:00" :UPDATED-AT "" :PATHSTR "about.md")
- #S(CONTENT :NAME "index" :TEMPLATE-NAME "markdown-template" :CREATED-AT "2021-03-30T21:00:00+09:00" :UPDATED-AT "" :PATHSTR "index.html")
- #S(CONTENT :NAME "style" :TEMPLATE-NAME NIL :CREATED-AT "2021-03-30T21:00:00+09:00" :UPDATED-AT "" :PATHSTR "assets/style.css")) 
+(#S(CONTENT
+    :NAME "works"
+    :TEMPLATE-NAME "markdown-template"
+    :CREATED-AT "2021-04-04T22:02:52.780479+09:00"
+    :UPDATED-AT ""
+    :PATHSTR "works.md")
+ #S(CONTENT
+    :NAME "about"
+    :TEMPLATE-NAME "markdown-template"
+    :CREATED-AT "2021-04-04T22:02:45.564890+09:00"
+    :UPDATED-AT ""
+    :PATHSTR "about.md")
+ #S(CONTENT
+    :NAME "index"
+    :TEMPLATE-NAME NIL
+    :CREATED-AT "2021-04-04T22:02:37.868485+09:00"
+    :UPDATED-AT ""
+    :PATHSTR "index.html")
+ #S(CONTENT
+    :NAME "style"
+    :TEMPLATE-NAME NIL
+    :CREATED-AT "2021-04-04T22:02:33.532297+09:00"
+    :UPDATED-AT ""
+    :PATHSTR "assets/style.css")) 
