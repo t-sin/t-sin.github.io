ecec66067985d6cb4a35eb3d678ca3554b84742a
t-sin <shinichi.tanaka45@gmail.com>
2021-04-08 19:20:06 +0900

    Fix

--- .asha/blog/articles.lisp
+++ .asha/blog/articles.lisp
@@ -7,14 +7,14 @@
    :INDEX-TEMPLATE-NAME "index-template"
    :ARTICLES (#S(CONTENT
                  :NAME "blog2"
-                 :TEMPLATE-NAME "markdown-template"
+                 :TEMPLATE-NAME ""
                  :CREATED-AT "2021-04-08T15:10:11.836651+09:00"
                  :UPDATED-AT ""
                  :PATHSTR "blog/blog2.md"
                  :TAGS ("SECD機械" "Lisp"))
               #S(CONTENT
                  :NAME "test"
-                 :TEMPLATE-NAME "markdown-template"
+                 :TEMPLATE-NAME ""
                  :CREATED-AT "2021-04-08T15:10:07.004299+09:00"
                  :UPDATED-AT ""
                  :PATHSTR "blog/test.md"
