b0e42d9980e5e64efaa6c8ae941d5724b98d127a
t-sin <shinichi.tanaka45@gmail.com>
2021-04-08 16:32:53 +0900

    Publish: test

--- .asha/blog/articles.lisp
+++ .asha/blog/articles.lisp
@@ -1,2 +1,21 @@
 
-#S(ARTICLE-SET :NAME "blog" :TEMPLATE-NAME "markdown-template" :ARTICLES NIL) 
+#S(ARTICLE-SET
+   :NAME "blog"
+   :TITLE "技術なこと"
+   :TEMPLATE-NAME "markdown-template"
+   :TAG-TEMPLATE-NAME "tag-template"
+   :INDEX-TEMPLATE-NAME "index-template"
+   :ARTICLES (#S(CONTENT
+                 :NAME "blog2"
+                 :TEMPLATE-NAME "markdown-template"
+                 :CREATED-AT "2021-04-08T15:10:11.836651+09:00"
+                 :UPDATED-AT ""
+                 :PATHSTR "blog/blog2.md"
+                 :TAGS ("SECD機械" "Lisp"))
+              #S(CONTENT
+                 :NAME "test"
+                 :TEMPLATE-NAME "markdown-template"
+                 :CREATED-AT "2021-04-08T15:10:07.004299+09:00"
+                 :UPDATED-AT ""
+                 :PATHSTR "blog/test.md"
+                 :TAGS ("Lisp" "プログラミング"))))
--- .asha/templates.lisp
+++ .asha/templates.lisp
@@ -1,4 +1,5 @@
 
-(#S(TEMPLATE :NAME "tag-template" :PATHSTR "tag-template.html")
+(#S(TEMPLATE :NAME "index-template" :PATHSTR "blog/index-template.html")
+ #S(TEMPLATE :NAME "tag-template" :PATHSTR "tag-template.html")
  #S(TEMPLATE :NAME "markdown-template" :PATHSTR "markdown-template.html")
  #S(TEMPLATE :NAME "template" :PATHSTR "template.html")) 
