f230117aee0e170d16e457defbb51ca6f7664193
t-sin <shinichi.tanaka45@gmail.com>
2021-04-11 00:30:10 +0900

    Add techblog styles as contents

--- .asha/contents.lisp
+++ .asha/contents.lisp
@@ -1,5 +1,12 @@
 
 (#S(CONTENT
+    :NAME "techblog/style.css"
+    :TEMPLATE-NAME NIL
+    :CREATED-AT "2021-04-10T23:05:42.299882+09:00"
+    :UPDATED-AT ""
+    :PATHSTR "techblog/style.css"
+    :TAGS NIL)
+ #S(CONTENT
     :NAME "works"
     :TEMPLATE-NAME "markdown-template"
     :CREATED-AT "2021-04-04T22:02:52.780479+09:00"
