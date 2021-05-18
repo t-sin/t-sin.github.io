5e88ef61eba17e23d6858332d3f0cf15b97da170
t-sin <shinichi.tanaka45@gmail.com>
2021-05-18 22:51:47 +0900

    Fix filename

--- .asha/techblog/articles.lisp
+++ .asha/techblog/articles.lisp
@@ -5,10 +5,10 @@
    :TEMPLATE-NAME "techblog.article-template"
    :TAG-TEMPLATE-NAME "techblog.tag-template"
    :INDEX-TEMPLATE-NAME "techblog.index-template"
-                 :NAME "2020-04-10_static-site-generator"
    :ARTICLES (#S(ASHA:CONTENT
+                 :NAME "2021-04-10_static-site-generator"
                  :TEMPLATE-NAME NIL
                  :CREATED-AT "2021-04-10T22:45:03.392272+09:00"
                  :UPDATED-AT ""
-                 :PATHSTR "techblog/2020-04-10_static-site-generator.md"
+                 :PATHSTR "techblog/2021-04-10_static-site-generator.md"
                  :TAGS ("プログラミング" "静的サイトジェネレータ" "Common Lisp" "Lisp" "自作"))))
