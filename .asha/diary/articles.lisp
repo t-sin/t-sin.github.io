3726c0d9214a4337dbd3ba53e67a8257a7153d49
t-sin <shinichi.tanaka45@gmail.com>
2021-05-18 22:47:30 +0900

    Follow exported Asha's structures

--- .asha/diary/articles.lisp
+++ .asha/diary/articles.lisp
@@ -1,11 +1,11 @@
 
-#S(ARTICLE-SET
+#S(ASHA:ARTICLE-SET
    :NAME "diary"
    :TITLE "日々のこと"
    :TEMPLATE-NAME "diary.article-template"
    :TAG-TEMPLATE-NAME "diary.tag-template"
    :INDEX-TEMPLATE-NAME "diary.index-template"
-   :ARTICLES (#S(CONTENT
+   :ARTICLES (#S(ASHA:CONTENT
                  :NAME "2021-04-14_tebanaka-horohoro-ni"
                  :TEMPLATE-NAME NIL
                  :CREATED-AT "2021-04-14T08:42:58.788372+09:00"
--- .asha/techblog/articles.lisp
+++ .asha/techblog/articles.lisp
@@ -1,12 +1,12 @@
 
-#S(ARTICLE-SET
+#S(ASHA:ARTICLE-SET
    :NAME "techblog"
    :TITLE "技術の記録"
    :TEMPLATE-NAME "techblog.article-template"
    :TAG-TEMPLATE-NAME "techblog.tag-template"
    :INDEX-TEMPLATE-NAME "techblog.index-template"
-   :ARTICLES (#S(CONTENT
                  :NAME "2020-04-10_static-site-generator"
+   :ARTICLES (#S(ASHA:CONTENT
                  :TEMPLATE-NAME NIL
                  :CREATED-AT "2021-04-10T22:45:03.392272+09:00"
                  :UPDATED-AT ""
