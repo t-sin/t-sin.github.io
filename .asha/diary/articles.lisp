72a28f6243b815526303a1426223b5496fb66542
t-sin <shinichi.tanaka45@gmail.com>
2021-06-15 19:03:23 +0900

    Save website

--- .asha/diary/articles.lisp
+++ .asha/diary/articles.lisp
@@ -1,11 +1,11 @@
 
-#S(ASHA:ARTICLE-SET
+#S(ASHA.WEBSITE:ARTICLE-SET
    :NAME "diary"
    :TITLE "日々のこと"
    :TEMPLATE-NAME "diary.article-template"
    :TAG-TEMPLATE-NAME "diary.tag-template"
    :INDEX-TEMPLATE-NAME "diary.index-template"
-   :ARTICLES (#S(ASHA:CONTENT
+   :ARTICLES (#S(ASHA.WEBSITE:CONTENT
                  :NAME "2021-04-14_tebanaka-horohoro-ni"
                  :TEMPLATE-NAME NIL
                  :CREATED-AT "2021-04-14T08:42:58.788372+09:00"
--- .asha/techblog/articles.lisp
+++ .asha/techblog/articles.lisp
@@ -1,25 +1,32 @@
 
-#S(ASHA:ARTICLE-SET
+#S(ASHA.WEBSITE:ARTICLE-SET
    :NAME "techblog"
    :TITLE "技術の記録"
    :TEMPLATE-NAME "techblog.article-template"
    :TAG-TEMPLATE-NAME "techblog.tag-template"
    :INDEX-TEMPLATE-NAME "techblog.index-template"
-   :ARTICLES (#S(ASHA:CONTENT
+   :ARTICLES (#S(ASHA.WEBSITE:CONTENT
+                 :NAME "2021-06-15-minecraft-server"
+                 :TEMPLATE-NAME NIL
+                 :CREATED-AT "2021-06-15T19:00:02.251230+09:00"
+                 :UPDATED-AT ""
+                 :PATHSTR "techblog/2021-06-15-minecraft-server.md"
+                 :TAGS ("ゲーム" "Minecraft" "IaaS" "クラウド"))
+              #S(ASHA.WEBSITE:CONTENT
                  :NAME "2021-05-28-game-engine-love"
                  :TEMPLATE-NAME NIL
                  :CREATED-AT "2021-05-28T09:43:26.014764+09:00"
                  :UPDATED-AT ""
                  :PATHSTR "techblog/2021-05-28-game-engine-love.md"
                  :TAGS ("プログラミング" "Lua" "ゲーム" "ゲームエンジン" "自作"))
-              #S(ASHA:CONTENT
+              #S(ASHA.WEBSITE:CONTENT
                  :NAME "2021-05-19_invader-game"
                  :TEMPLATE-NAME NIL
                  :CREATED-AT "2021-05-19T00:30:35.402697+09:00"
                  :UPDATED-AT ""
                  :PATHSTR "techblog/2021-05-19_invader-game.md"
                  :TAGS ("プログラミング" "ゲーム" "円城塔" "自作"))
-              #S(ASHA:CONTENT
+              #S(ASHA.WEBSITE:CONTENT
                  :NAME "2021-04-10_static-site-generator"
                  :TEMPLATE-NAME NIL
                  :CREATED-AT "2021-04-10T22:45:03.392272+09:00"
