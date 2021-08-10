4efcc8ae981e915c6ece51ab14854f8c99d2287b
t-sin <shinichi.tanaka45@gmail.com>
2021-08-10 10:19:48 +0900

    Article: not healthy

--- .asha/diary/articles.lisp
+++ .asha/diary/articles.lisp
@@ -6,6 +6,13 @@
    :TAG-TEMPLATE-NAME "diary.tag-template"
    :INDEX-TEMPLATE-NAME "diary.index-template"
    :ARTICLES (#S(ASHA.WEBSITE:CONTENT
+                 :NAME "2021-08-10_not-healty"
+                 :TEMPLATE-NAME NIL
+                 :CREATED-AT "2021-08-10T10:18:00.209778+09:00"
+                 :UPDATED-AT ""
+                 :PATHSTR "diary/2021-08-10_not-healty.md"
+                 :TAGS ("健康"))
+              #S(ASHA.WEBSITE:CONTENT
                  :NAME "2021-07-25-back-to-the-chicken"
                  :TEMPLATE-NAME NIL
                  :CREATED-AT "2021-07-25T23:26:08.415746+09:00"
